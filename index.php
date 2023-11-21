a366617353c016d629dc90e70eed90766d137fe3
dadie225 <151544415+dadie225@users.noreply.github.com>
2023-11-21 11:13:57 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,9 @@
-<?php phpinfo();?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Test PHP</title>
+    </head>
+    <body>
+        <?php echo '<p>Bonjour le monde, Bonjour Elysé</p>'; ?>
+    </body>
+</html>
